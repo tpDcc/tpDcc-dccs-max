c6ec75e3c56eae8e7ad184097990fb2261ca72c3
Tomi Poveda <tpoveda@cgart3d.com>
2019-04-28 03:38:34 +0200

    Added some more implementations to Max DCC

--- source/tpMaxLib/core/dcc.py
+++ source/tpMaxLib/core/dcc.py
@@ -7,42 +7,96 @@ Module that contains DCC functionality for 3ds Max
 
 from __future__ import print_function, division, absolute_import
 
+import MaxPlus
+
 import tpDccLib
 from tpDccLib.abstract import dcc as abstract_dcc
 
 
-from tpMaxLib.core import gui, helpers, scene, directory
+from tpMaxLib.core import gui, helpers, scene, directory, viewport
 
 
 class MaxDcc(abstract_dcc.AbstractDCC, object):
 
     @staticmethod
     def get_name():
+        """
+        Returns the name of the DCC
+        :return: str
+        """
+
         return tpDccLib.Dccs.Max
 
     @staticmethod
-    def get_main_window():
-        return gui.get_max_window()
-
-    @staticmethod
     def get_version():
+        """
+        Returns version of the DCC
+        :return: int
+        """
+
         return helpers.get_max_version()
 
     @staticmethod
-    def new_scene(force=True, do_save=True):
-        scene.new_scene(force=force, do_save=do_save)
+    def get_main_window():
+        """
+        Returns Qt object that references to the main DCC window
+        :return:
+        """
+
+        return gui.get_max_window()
 
     @staticmethod
-    def select_file(caption, filters, start_dir=None):
+    def clear_selection():
         """
-        Opens a select file dialog with DCC dialog
-        :param caption: str, caption of the dialog
-        :param filters: str, filter to use
-        :param start_dir: str, start directory of the dialog
-        :return: str, selected path
+        Clears current scene selection
         """
 
-        return directory.get_file(caption=caption, filters=filters)
+        MaxPlus.SelectionManager.ClearNodeSelection()
+
+    @staticmethod
+    def new_file(force=True):
+        """
+        Creates a new file
+        :param force: bool
+        """
+
+        scene.new_scene(force=force)
+
+    @staticmethod
+    def select_file_dialog(title, start_directory=None, pattern=None):
+        """
+        Shows select file dialog
+        :param title: str
+        :param start_directory: str
+        :param pattern: str
+        :return: str
+        """
+
+        return directory.get_file(caption=title, start_directory=start_directory, filters=pattern)
+
+    @staticmethod
+    def refresh_viewport():
+        """
+        Refresh current DCC viewport
+        """
+
+        viewport.force_redraw()
+
+    @staticmethod
+    def enable_undo():
+        """
+        Enables undo functionality
+        """
+
+        return False
+
+    @staticmethod
+    def disable_undo():
+        """
+        Disables undo functionality
+        """
+
+        return False
 
 
 tpDccLib.Dcc = MaxDcc
